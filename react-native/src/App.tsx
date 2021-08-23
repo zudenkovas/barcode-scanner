57a812292ce26929de9c7994728f816562297e82
Christian Justus <christian.justus@hybridheroes.de>
2021-08-23 12:38:28 +0200

    fix(react-native): force light mode

--- react-native/src/App.tsx
+++ react-native/src/App.tsx
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import { configureStore } from "./store";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { Provider as PaperProvider } from "react-native-paper";
 
 const Stack = createStackNavigator();
 const store = configureStore();
@@ -14,19 +13,17 @@ const store = configureStore();
 export default () => (
   <Provider store={store}>
     <SafeAreaProvider>
-      <PaperProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            initialRouteName="Home"
-            screenOptions={{
-              headerShown: false
-            }}
-          >
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Camera" component={CameraScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false
+          }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Camera" component={CameraScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   </Provider>
 );
